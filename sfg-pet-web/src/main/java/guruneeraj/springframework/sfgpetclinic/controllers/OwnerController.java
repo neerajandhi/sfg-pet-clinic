4b18bcc47a7181efe69d04dbdaa2f9327a323516
Neeraj Kumar <neerajandhi@gmail.com>
2024-05-09 08:11:35 +0530

    Implemented Owners Index and Controller

---
+++ sfg-pet-web/src/main/java/guruneeraj/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package guruneeraj.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"owners","owners/index","owners/index.html"})
+    public String getOwners(){
+        return "owners/index";
+    }
+}
